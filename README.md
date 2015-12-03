39fbd34d7c5c4fe1131c3b2ac00faa7837097d2e
Léo Peltier <leo@boaterfly.com>
2015-12-03 16:47:01 +0100

    Download attachments.

--- README.md
+++ README.md
@@ -14,15 +14,25 @@ make
 
 ### 2. Export data from AgileZen.
 ```shell
-./agilezen-to-redmine export --agilezen-key=AGILEZEN-KEY file.name
+./agilezen-to-redmine export export --agilezen-key=AGILEZEN-KEY
 ```
 
 All projects, stories, and comments will be downloaded.  
 You can obtain your AgileZen API key in the 'developer' section of the [AgileZen
 settings](https://agilezen.com/settings).
 
+### 2. Download attachments from AgileZen.
+```shell
+./agilezen-to-redmine download-attachments export --user=USER --password=PASSWORD
+```
+
+The AgileZen API can't be used to download attachments so an acual user has to
+be spoofed.
+
 ## Notes
-No effort is made to reduce memory consumption. Your whole AgileZen dataset will
+No effort was made to reduce memory consumption. Your whole AgileZen dataset will
 be loaded multiple times in memory.  
+No effort was made to reduce the number of files in a single directory. If you
+have enough atachments to make your local FS break, congratulations.  
 There is no way to fetch all attachments at once or to know if a story has
 any, so **there will be one HTTP GET request per story**.
---
+++ src/Command/DownloadAttachments.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace AgileZenToRedmine\Command;
+
+use GuzzleHttp\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use AgileZenToRedmine\Api\AgileZen\Attachment;
+use AgileZenToRedmine\Api\AgileZen\Project;
+use AgileZenToRedmine\Api\AgileZen\Story;
+
+class DownloadAttachments extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('download-attachments')
+            ->setDescription('Download attachments from AgileZen.')
+            ->addArgument(
+                'output-dir',
+                InputArgument::REQUIRED,
+                'Where to read and write the exported data.'
+            )
+            ->addOption(
+                'user',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'AgileZen user.'
+            )
+            ->addOption(
+                'password',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'AgileZen password.'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $outputDir = $input->getArgument('output-dir');
+        if (!file_exists("$outputDir/agilezen.dat")) {
+            throw new \RuntimeException('No exported data found at given output dir.');
+        }
+
+        $attachmentsDir = "$outputDir/attachments";
+        if (!file_exists($attachmentsDir)) {
+            if (!mkdir($attachmentsDir, 0775)) {
+                throw new \RuntimeException('Unable to create directory for downloaded attachments.');
+            }
+        }
+
+        $projects = unserialize(file_get_contents("$outputDir/agilezen.dat"));
+        $progress = new ProgressBar($output, $this->getAttachmentSize($projects));
+        $output->writeln('Get authenticated client.');
+        $client = $this->getClient($input);
+
+        $output->writeln('Start downloading files.');
+        foreach ($projects as $project) {
+            foreach ($project->stories as $story) {
+                foreach ($story->attachments as $attachment) {
+                    $path = "$attachmentsDir/{$attachment->id}";
+                    $this->downloadAttachment($client, $path, $project, $story, $attachment);
+                    $progress->advance($attachment->sizeInBytes);
+                }
+            }
+        }
+
+        $progress->finish();
+        $output->writeln('');
+    }
+
+    private function downloadAttachment(Client $client, $path, Project $project, Story $story, Attachment $attachment)
+    {
+        if (file_exists($path) && filesize($path) === $attachment->sizeInBytes) {
+            return;
+        }
+
+        $uri = "project/{$project->id}"
+            . "/story/{$story->id}"
+            . "/attachment/{$attachment->id}"
+            . "/download/{$attachment->fileName}"
+        ;
+
+        $out = fopen($path, 'w+');
+        $client->get($uri, [
+            'sink' => $out
+        ]);
+    }
+
+    /**
+     * @param Project[] $projects
+     * @return int
+     */
+    private function getAttachmentSize(array $projects)
+    {
+        $totalSize = 0;
+
+        foreach ($projects as $project) {
+            foreach ($project->stories as $story) {
+                $totalSize += array_sum(collection_column($story->attachments, 'sizeInBytes'));
+            }
+        }
+
+        return $totalSize;
+    }
+
+
+    /**
+     * @return GuzzleHttp\Client authentificated client ready to download
+     * attachments.
+     */
+    private function getClient(InputInterface $input)
+    {
+        $user     = $input->getOption('user');
+        $password = $input->getOption('password');
+        foreach (['user', 'password'] as $k) {
+            if (!is_string($$k) || strlen($$k) <= 0) { // sue me
+                throw new \RuntimeException("Option --$k is required.");
+            }
+        }
+
+        $client = new Client([
+            'base_uri' => 'https://agilezen.com/',
+            'cookies' => true,
+        ]);
+        $client->get('login');
+        $client->request('POST', 'login', [
+            'headers' => [
+                'X-Requested-With' => 'XMLHttpRequest',
+                'Accept' => 'application/json',
+                'Accept-Encoding' => 'gzip',
+            ],
+            'form_params' => [
+                'userName' => $user,
+                'password' => $password,
+                'timezoneOffset' => 1, // arbitrary
+            ]
+        ]);
+
+        return $client;
+    }
+}
