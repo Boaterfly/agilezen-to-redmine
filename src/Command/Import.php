461d33135befee716454aaba3cbbb121adaffa2e
Léo Peltier <contact@leo-peltier.fr>
2015-12-04 14:17:03 +0100

    Import users.

--- src/Command/Import.php
+++ src/Command/Import.php
@@ -4,16 +4,21 @@ namespace AgileZenToRedmine\Command;
 
 use Redmine\Client;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+
 use AgileZenToRedmine\Dump;
 
 class Import extends Command
 {
     /// @var Redmine\Client
-    private $api;
+    private $redmine;
+
+    /// @var OutputInterface
+    private $output;
 
     protected function configure()
     {
@@ -44,6 +49,8 @@ class Import extends Command
 
     protected function initialize(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
+
         $url = $input->getOption('redmine-url');
         $key = $input->getOption('redmine-key');
 
@@ -55,11 +62,52 @@ class Import extends Command
             throw new \RuntimeException('Invalid URL for --redmine-url.');
         }
 
-        $this->api = new Client($url, $key);
+        $this->redmine = new Client($url, $key);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dump = Dump::load($input->getOption('output-dir'));
+
+        $this->createUsers($dump->getUsers());
+    }
+
+    /**
+     * Create Redmine users if they don't already exist (email as primary key).
+     *
+     * @param User[] $users
+     */
+    private function createUsers(array $users)
+    {
+        $this->output->writeln('Create users.');
+        $progress = new ProgressBar($this->output, count($users));
+        $skipped = 0;
+        $redmineUsers = array_column(
+            $this->redmine->api('user')->all()['users'],
+            'mail'
+        );
+
+        foreach ($users as $user) {
+            if (in_array($user->email, $redmineUsers, true)) {
+                $skipped += 1;
+                $progress->advance();
+                continue;
+            }
+
+            // Mandatory parameters, can't be an empty string.
+            $names = explode(' ', $user->name, 2) + ['NoFirstName', 'NoLastName'];
+
+            $this->redmine->api('user')->create([
+                'login'     => explode('@', $user->email)[0],
+                'firstname' => $names[0],
+                'lastname'  => $names[1],
+                'mail'      => $user->email
+            ]);
+
+            $progress->advance();
+        }
+
+        $progress->finish();
+        $this->output->writeln("\nDone creating users, skipped: $skipped");
     }
 }
