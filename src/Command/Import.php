c3aef9cda6111dcba6eca646a967f20dfd6c829d
Léo Peltier <contact@leo-peltier.fr>
2015-12-04 12:45:43 +0100

    Bootstrap import command.

---
+++ src/Command/Import.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AgileZenToRedmine\Command;
+
+use Redmine\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Import extends Command
+{
+    /// @var Redmine\Client
+    private $api;
+
+    protected function configure()
+    {
+        $this
+            ->setName('import')
+            ->setDescription('Import exported data from AgileZen into Redmine')
+            ->addArgument(
+                'output-dir',
+                InputArgument::REQUIRED,
+                'Where to read the exported data.'
+            )
+            ->addOption(
+                'redmine-url',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Redmine HTTP URL.'
+            )
+            ->addOption(
+                'redmine-key',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Redmine API key.'
+            )
+        ;
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $url = $input->getOption('redmine-url');
+        $key = $input->getOption('redmine-key');
+
+        if (strlen($url) <= 0 || strlen($key) <= 0) {
+            throw new \RuntimeException('Both --redmine-url and --redmine-key are required.');
+        }
+
+        if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+            throw new \RuntimeException('Invalid URL for --redmine-url.');
+        }
+
+        $this->api = new Client($url, $key);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    }
+}
